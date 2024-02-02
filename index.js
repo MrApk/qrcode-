2de26c8b90e3831c4007ad78b178d03cb5367e37
Apk <111860693+MrApk@users.noreply.github.com>
2024-02-02 19:48:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/api/generate', async (req, res) => {
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, '404.html'));
+    res.status(404).sendFile(__dirname, '404.html');
 });
 
 app.listen(port, () => {
